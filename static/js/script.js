240581ae6e5dd029b094c706e74c485ca44a322a
Brett Weir <brett@lamestation.com>
2014-08-16 07:32:00 -0700

    Removed all pages except for the front page; added donate button, pre-order button, search button, social media buttons, reorganized nav bar; deleted dead code.

--- static/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function()
-{
-	var toc = $("#toc").tocify({ selectors: "h2,h3" }).data("tocify");
-	                 
-	toc.setOption("showEffect", "none");
-	toc.setOption("showAndHide", "false");
-	toc.setOption("extendPage", "false");
-})
